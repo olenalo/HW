344daaa43ac3e70b2315953c87155023ceeef618
olenalo <uleta@ukr.net>
2018-10-15 00:08:28 +0300

    Add StandardMultiplication class (skeleton) and update sout of KaratsubaAlgorithm

---
+++ src/com/company/StandardMultiplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class StandardMultiplication {
+
+    public static long multiplyByGradeSchoolApgorithm(long x, long y) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
