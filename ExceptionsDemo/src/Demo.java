ad108e94e290dec63217562d3b6ce00dd1c720e2
olenalo <uleta@ukr.net>
2018-10-27 20:26:53 +0300

    HW13: update domain model, add the code leading to exceptions
    Add a case provoking NumberFormatException
    Remove unnecessary entities (workshops, talks)
    Add another case provoking NullPointerException
    Add price to conference
    Fix a case provoking ArrayIndexOutOfBoundsException (now it really occurs :) )

--- ExceptionsDemo/src/Demo.java
+++ ExceptionsDemo/src/Demo.java
@@ -23,32 +23,53 @@ public class Demo {
         Conference javaCon = new JavaConference(
                 "JavaCon Europe",
                 "Jabba the Hutt",
-                "Tower of Hanoi");
+                "Tower of Hanoi",
+                550);
         Conference pyCon = new PythonConference(
                 "PyCon Ukraine",
-                "Monty Python");
+                "Monty Python",
+                450);
         Conference dsCon = new DataScienceConference(
                 "Data Science World Conference",
                 "UCU",
-                "Robots-Programmers Expo");
+                "Robots-Programmers Expo",
+                1000);
 
         conferences[0] = javaCon;
         conferences[1] = pyCon;
         conferences[2] = dsCon;
-        String[] giveAwayCandidates = new String[] {"Vova", "OPersian", "Lola", "Linda", "Max", "Peter"};
+        String[] giveAwayContestCandidates = new String[] {"Vova", "OPersian", "Lola", "Linda", "Max", "Peter"};
+
+        // Update the price (organizers decided to make JavaCon and PyCon prices equal taking the JavaCon price)
+        // FIXME: NumberFormatException (cannot cast string of such format to numeric type)
+        pyCon.setPrice(Double.valueOf(javaCon.showPrice()));
 
         for (Conference conference: conferences) {
             System.out.println(conference);
-            System.out.println(conference.getTalks());
-            System.out.println(conference.getWorkshops());
+            System.out.println("The talks are as follows: ");
+
+            // FIXME: NullPointerException could occur here and below
+            // if a conference has no `talks`
+            for (String talk: conference.getTalks()) {
+                System.out.println(talk);
+            }
+            System.out.println("The workshops are as follows: ");
+            for (String workshop: conference.getWorkshops()) {
+                System.out.println(workshop);
+            }
+
             System.out.println(makeTechRiddle(conference));
             System.out.println(showExhibitionArea(conference));
+
             System.out.println("The winners of free tickets are: ");
-            for (String winner: conference.pickFreeTicketsWinners(giveAwayCandidates, 2)) {
+            for (String winner: conference.pickFreeTicketsWinners(giveAwayContestCandidates, 2)) {
                 System.out.println(winner);
             }
+            System.out.println("The price is " + conference.showPrice());
             System.out.println("---------------");
         }
+
+
 
 
     }
