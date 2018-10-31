ddafbe518abc411022ec6929ffb0c2c828424959
olenalo <uleta@ukr.net>
2018-10-31 20:20:06 +0200

    HW12: Make it possible for multiple dishes to be ordered

--- Restaurant/src/Demo.java
+++ Restaurant/src/Demo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
@@ -11,61 +12,80 @@ public class Demo {
         }
         System.out.println("--------------------------------");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please select the dish (enter the number): ");
-        // TODO: make it possible for multiple items to be chosen
-        String orderedDishIndex = sc.next();
         Order order = new Order();
-        order.addDish(orderedDishIndex);
+        Scanner sc = new Scanner(System.in);
+        int orderedDishesNumber = 0;
+        while (orderedDishesNumber <= Order.MAX_DISHES_NUMBER) {
+            System.out.println("Please select the dish (enter the number): ");
+            String orderedDishIndex = sc.next();
+            order.addDish(orderedDishIndex);
+            orderedDishesNumber++;
+            System.out.println("Would you like to order more dishes? y/n ");
+            String continueOrderDecision = sc.next();
+            if (continueOrderDecision.equals("n") || continueOrderDecision.equals("N")) {
+                break;
+            }
+        }
         System.out.println("--------------");
 
         Waiter waiter = new Waiter("Mila", "Jovovich");
-
         // TODO: figure out how to assign to the order (using factory below? really?)
         Cook cook1 = new Cook("Marylin", "Manson");
         Cook cook2 = new Cook("Vanya", "Tomych");
         DishWasher dishwasher = new DishWasher("Tina", "Tinova");
 
         waiter.takeOrder(order);
+        System.out.println("You ordered: ");
+        for (String[] dishDescr: order.getDishes()) {
+            System.out.println(dishDescr[1]);
+        }
+        System.out.println("Thank you! Your order will be ready soon.");
+        System.out.println("--------------------------------");
 
-        for (String[] orderedMenuItem: order.getDishes()) {
-            if (orderedMenuItem[2] != null) {
-                switch (orderedMenuItem[2]) {
-                    case "Soup":
-                        dishFactory = new SoupFactory();
-                        break;
-                    case "MainDish":
-                        dishFactory = new MainDishFactory();
-                        break;
-                    case "Dessert":
-                        dishFactory = new DessertFactory();
-                        break;
+        if (order.getDishes() != null) {
+            for (String[] orderedMenuItem : order.getDishes()) {
+                System.out.println("Working on " + orderedMenuItem[1]);
+                if (orderedMenuItem[2] != null) {
+                    switch (orderedMenuItem[2]) {
+                        case "Soup":
+                            dishFactory = new SoupFactory();
+                            break;
+                        case "MainDish":
+                            dishFactory = new MainDishFactory();
+                            break;
+                        case "Dessert":
+                            dishFactory = new DessertFactory();
+                            break;
+                    }
                 }
+
+                if (dishFactory != null) {
+                    DishBuilder dishBuilder = dishFactory.cook(orderedMenuItem[0]);
+
+                    dishBuilder
+                            .environment(DishPreparationStep.valueOf("ENVIRONMENT").description, order, cook1)
+                            .ingredients(DishPreparationStep.valueOf("INGREDIENTS").description, order, cook1)
+                            .technology(DishPreparationStep.valueOf("TECHNOLOGY").description, order, cook2)
+                            .utensil(DishPreparationStep.valueOf("UTENSIL").description, order, dishwasher)
+                            .design(DishPreparationStep.valueOf("DESIGN").description, order, cook2);
+
+                    System.out.println("--------------");
+                    Dish dish = dishBuilder.createDish();
+                    waiter.serveDish(order, dish);
+                    System.out.println("--------------");
+                }
+
+                System.out.println("===================================");
+
             }
         }
 
-        if (dishFactory != null) {
-            DishBuilder dishBuilder = dishFactory.cook(orderedDishIndex);
-
-            dishBuilder
-                    .environment(DishPreparationStep.valueOf("ENVIRONMENT").description, order, cook1)
-                    .ingredients(DishPreparationStep.valueOf("INGREDIENTS").description, order, cook1)
-                    .technology(DishPreparationStep.valueOf("TECHNOLOGY").description, order, cook2)
-                    .utensil(DishPreparationStep.valueOf("UTENSIL").description, order, dishwasher)
-                    .design(DishPreparationStep.valueOf("DESIGN").description, order, cook2);
-
-            System.out.println("--------------");
-            Dish dish = dishBuilder.createDish();
-            waiter.serveDish(order, dish);
-            System.out.println("--------------");
-
-            System.out.println("Information on the order: ");
-            for (Cook cook : order.getResponsibleCooks()) {
-                System.out.println("Responsible cook: " + cook);
-            }
-            System.out.println("Responsible dishwasher: " + order.getResponsibleDishwasher());
-            System.out.println("Responsible waiter: " + order.getResponsibleWaiter());
+        System.out.println("Information on the order: ");
+        for (Cook cook : order.getResponsibleCooks()) {
+            System.out.println("Responsible cook: " + cook);
         }
+        System.out.println("Responsible dishwasher: " + order.getResponsibleDishwasher());
+        System.out.println("Responsible waiter: " + order.getResponsibleWaiter());
 
 
     }
--- Restaurant/src/Order.java
+++ Restaurant/src/Order.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Order {
 
+    public static final int MAX_DISHES_NUMBER = 5;
     private List<String[]> dishes;
     private List<Cook> responsibleCooks;
     private Waiter responsibleWaiter;
@@ -35,7 +36,7 @@ public class Order {
 
     public void addDish(String dishIndex) {
         for (Menu dish: Menu.values()) {
-            if (dishIndex.equals(dish.dishDescriptors[0])){
+            if (dishIndex.equals(dish.dishDescriptors[0]) && this.dishes.size() <= MAX_DISHES_NUMBER){
                 this.dishes.add(new String[]{dish.dishDescriptors[0], dish.dishDescriptors[1], dish.dishDescriptors[2]});
                 System.out.printf("You ordered '%s', thank you!\n", dish.dishDescriptors[1]);
             }
