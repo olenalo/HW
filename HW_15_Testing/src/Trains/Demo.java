fdb37454f2d3d0f44bc7277acf89f499df87fdb2
olenalo <uleta@ukr.net>
2018-11-06 11:58:45 +0200

    HW15: Update tests, add FIXMEs

--- HW_15_Testing/src/Trains/Demo.java
+++ HW_15_Testing/src/Trains/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
         // Train train = new Train(5, 0, 0);
         // Train train = new Train(0, 2, 0);
         // Train train = new Train(0, 0, 4);
+        // Train train = new Train(2, 0, 0);
 
         train.setItinerary("Kharkiv", "Kyiv");
 
