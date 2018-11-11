f9685adb32427c1b315a795061812833103fc5d2
olenalo <olenalo@users.noreply.github.com>
2018-11-12 00:59:10 +0200

    HW16: Implement custom Linked List (finish CW) and add tests (#8)
    * Add classwork #15 code (custom Linked List) and fix the code
    * Implement more methods of the custom Linked List
    * Add unit tests

---
+++ HW_16_CustomLinkedList/src/MyOwnList.java
@@ -0,0 +1,367 @@
+import java.util.*;
+import java.util.function.Consumer;
+
+public class MyOwnList<T> implements List<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
+    private class MyOwnIterator implements Iterator<T> {
+        private Node<T> lastReturned;
+        private Node<T> next;
+        private int nextIndex;
+
+        MyOwnIterator(int index) {
+            next = (index == size) ? null : getNode(index);
+            nextIndex = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextIndex < size;
+        }
+
+        @Override
+        public T next() {
+            checkIndex(nextIndex);
+            if (!hasNext())
+                throw new NoSuchElementException();
+            lastReturned = next;
+            next = next.getNext();
+            nextIndex++;
+            return lastReturned.getElement();
+        }
+
+        @Override
+        public void remove() {
+            // TODO implement
+            throw  new UnsupportedOperationException();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer action) {
+            // TODO implement
+            throw  new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean add(T t) {
+        Node<T> newElement = new Node<>(t, this.last, null);
+        Node<T> last = this.last;
+        this.last = newElement;
+        if (last == null) {
+            this.first = newElement;
+        } else {
+            last.setNext(newElement);
+        }
+        this.size++;
+        return true;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (this.isEmpty() || index == this.size) {
+            checkIndex(index);
+            this.add(element);
+        } else {
+            Node<T> current = getNode(index);
+            Node<T> addingElement = new Node<>(element, current.getPrev(), current);
+            this.size++;
+            current.getPrev().setNext(addingElement);
+            current.setPrev(addingElement);
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        for(T t: c){
+            add(t);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (this.isEmpty() || index == this.size) {
+            checkIndex(index);
+            this.addAll(c);
+        } else {
+            for (T element : c) {
+                this.add(index, element);
+                index++;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T el = null;
+        Node<T> current = getNode(index);
+        if (current != null) {
+            el = current.getElement();
+        }
+        return el;
+    }
+
+    public T getFirst() {
+        Node<T> node = this.first;
+        if (node == null)
+            throw new NoSuchElementException();
+        return node.getElement();
+    }
+
+    public T getLast() {
+        Node<T> node = this.last;
+        if (node == null)
+            throw new NoSuchElementException();
+        return node.getElement();
+    }
+
+    private Node<T> getNode(int index) {
+        checkIndex(index);
+        int counter = 0;
+        Node<T> current = this.first;
+        while(counter != index){
+            counter++;
+            current = current.getNext();
+        }
+        return current;
+    }
+
+    private Node<T> getNode(Object o) {
+        Node<T> current = this.first;
+        if (current != null) {
+            int counter = 0;
+            while (counter < this.size()) {
+                if(current.getElement().equals(o)) {
+                    return current;
+                }
+                current = current.getNext();
+                counter++;
+            }
+        }
+        return current;
+    }
+
+    private void checkIndex(int index) {
+        if (this.isEmpty()) {
+            if (index > 0) {
+                throw new IndexOutOfBoundsException();
+            }
+        } else if (index > this.size) {
+                throw new IndexOutOfBoundsException();
+        }
+        if(index < 0){
+            throw new NegativeArraySizeException();
+        }
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = getNode(index);
+        T prevElement = current.getElement();
+        current.setElement(element);
+        return prevElement;
+    }
+
+    private void unlinkNode(Node<T> current, Node<T> prev, Node<T> next) {
+        if (prev == null) {
+            this.first = next;
+        } else {
+            prev.setNext(next);
+            current.setPrev(null);
+        }
+        if (next == null) {
+            this.last = prev;
+        } else {
+            next.setPrev(prev);
+            current.setNext(null);
+        }
+        current.setElement(null);
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = getNode(index);
+        Node<T> prev = current.getPrev();
+        Node<T> next = current.getNext();
+        T element = current.getElement();
+        unlinkNode(current, prev, next);
+        this.size--;
+        return element;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Node<T> current = getNode(o);
+        if (current == null) {
+            return false;
+        } else {
+            Node<T> prev = current.getPrev();
+            Node<T> next = current.getNext();
+            unlinkNode(current, prev, next);
+            this.size--;
+            return true;
+        }
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean result = false;
+        for (Object o: c) {
+            if (this.remove(o)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean result = false;
+        for (T element: this) {
+            // System.out.println(element);
+            if (!c.contains(element)) {
+                if(this.remove(element)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        int counter = 0;
+        Node<T> current = this.first;
+        while(current.getElement() != o){
+            counter++;
+            current = current.getNext();
+            if(counter == this.size){
+                return -1;
+            }
+        }
+        return counter;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        int counter = 1;
+        Node<T> current = this.last;
+        while(current.getElement() != o){
+            counter++;
+            current = current.getPrev();
+            if(counter == this.size){
+                return -1;
+            }
+        }
+
+        return this.size - counter;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (T element: this) {
+            if (o.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c.isEmpty()) {
+            return false;
+        }
+        for(Object o: c) {
+            if(!this.contains(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[this.size()];
+        for (int i = 0; i < this.size(); i++) {
+            array[i] = this.get(i);
+        }
+        return array;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        // TODO implement, considering this: https://stackoverflow.com/a/530289
+        throw  new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public MyOwnList<T> subList(int fromIndex, int toIndex) {
+        if (fromIndex >= this.size || toIndex >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        checkIndex(fromIndex);
+        checkIndex(toIndex);
+        MyOwnList<T> newList = new MyOwnList<>();
+        for (int i = fromIndex; i <= toIndex; i++) {
+            newList.add(this.get(i));
+        }
+        return newList;
+    }
+
+    @Override
+    public MyOwnIterator iterator() {
+        return new MyOwnIterator(0);
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        // TODO implement
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        // TODO implement
+        throw  new UnsupportedOperationException();
+    }
+
+}
---
+++ HW_16_CustomLinkedList/test/MyOwnListTest.java
@@ -0,0 +1,597 @@
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class MyOwnListTest {
+    // TODO ensure first and last elements do not have prev and next nodes respectively, use this: https://stackoverflow.com/a/34658
+    MyOwnList<Integer> list;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        list = new MyOwnList<>();
+    }
+
+    @org.junit.Test()
+    public void testIsEmpty() {
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testIsEmptyThrowsException() {
+        assertNull(list.get(0)); // `checkIndex()` throws an exception
+    }
+
+    @org.junit.Test
+    public void testAdd() {
+        assertTrue(list.add(1));
+        assertFalse(list.isEmpty());
+        assertEquals(1, list.size());
+        assertEquals(new Integer(1), list.getFirst());
+        assertEquals(list.getFirst(), list.getLast());
+
+        assertTrue(list.add(2));
+        assertEquals(2, list.size());
+        assertEquals(new Integer(1), list.getFirst());
+        assertEquals(new Integer(2), list.getLast());
+
+        assertTrue(list.add(3));
+        assertEquals(3, list.size());
+        assertEquals(new Integer(1), list.getFirst());
+        assertEquals(new Integer(3), list.getLast());
+    }
+
+    @org.junit.Test
+    public void testAddByIndex() {
+        list.add(0,5);
+        assertFalse(list.isEmpty());
+        assertEquals(new Integer(5), list.getFirst());
+
+        list.add(1,6);
+        assertEquals(new Integer(6), list.get(1));
+
+        list.add(2,7);
+        assertEquals(new Integer(7), list.get(2));
+        assertEquals(3, list.size());
+
+        // Add somewhere in the middle (non-empty)
+        list.add(1,99);
+        assertEquals(4, list.size());
+        assertEquals(new Integer(5), list.get(0));
+        assertEquals(new Integer(99), list.get(1));
+        assertEquals(new Integer(6), list.get(2));
+        assertEquals(new Integer(7), list.get(3));
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testAddByIndexWrongIndexFailure() {
+        list.add(1,5);
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testAddByIndexNegativeArraySizeException() {
+        list.add(-1,5);
+    }
+
+    @org.junit.Test
+    public void testAddAll() {
+        LinkedList<Integer> someList = new LinkedList<>();
+        someList.add(1);
+        someList.add(2);
+        someList.add(3);
+
+        list.addAll(someList);
+        assertEquals(3, list.size());
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(2), list.get(1));
+        assertEquals(new Integer(3), list.get(2));
+
+        list.addAll(someList);
+        assertEquals(6, list.size());
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(2), list.get(1));
+        assertEquals(new Integer(3), list.get(2));
+        assertEquals(new Integer(1), list.get(3));
+        assertEquals(new Integer(2), list.get(4));
+        assertEquals(new Integer(3), list.get(5));
+    }
+
+    @org.junit.Test
+    public void testAddAllFromEmptyList() {
+        list.addAll(new LinkedList<>());
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testAddAllByIndexFromEmptyList() {
+        list.addAll(0, new LinkedList<>());
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testAddAllByIndexToEmpyList() {
+        LinkedList<Integer> someList = new LinkedList<>();
+        someList.add(1);
+        someList.add(2);
+        someList.add(3);
+        // Add to an empty list
+        list.addAll(0, someList);
+        assertEquals(3, list.size());
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(2), list.get(1));
+        assertEquals(new Integer(3), list.get(2));
+    }
+
+    @org.junit.Test
+    public void testAddAllByIndexInTheMiddle() {
+        LinkedList<Integer> someList = new LinkedList<>();
+        someList.add(1);
+        someList.add(2);
+        someList.add(3);
+        list.addAll(0, someList);
+        // Add somewhere in the middle (non-empty)
+        list.addAll(1, someList);
+        assertEquals(6, list.size());
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(1), list.get(1));
+        assertEquals(new Integer(2), list.get(2));
+        assertEquals(new Integer(3), list.get(3));
+        assertEquals(new Integer(2), list.get(4));
+        assertEquals(new Integer(3), list.get(5));
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllByIndexWrongIndexFailure() {
+        list.addAll(1, new LinkedList<>());
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testAddAllByIndexNegativeArraySizeException() {
+        list.addAll(-1, new LinkedList<>());
+    }
+
+    @org.junit.Test
+    public void testSetSuccess() {
+        list.add(1);
+        list.set(0, 2);
+        assertEquals(new Integer(2), list.get(0));
+        assertEquals(1, list.size());
+
+        list.add(22);
+        list.set(1, 33);
+        assertEquals(new Integer(33), list.get(1));
+        assertEquals(new Integer(2), list.get(0));
+        assertEquals(2, list.size());
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testSetEmptyArrayFailure() {
+        list.set(0, 55);
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testSetWrongIndexFailure() {
+        list.add(1);
+        list.set(1, 2);
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testSetNegativeIndexFailure() {
+        list.add(1);
+        list.set(-1, 2);
+    }
+
+    @org.junit.Test
+    public void testClearSuccess() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.clear();
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testClearGetFirstFailure() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.clear();
+        assertTrue(list.isEmpty());
+        list.getFirst();
+    }
+
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testClearGetLastFailure() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.clear();
+        list.getLast();
+    }
+
+    @org.junit.Test
+    public void testFirstSuccess() {
+        list.add(1);
+        assertEquals(new Integer(1), list.getFirst());
+
+        list.add(2);
+        assertEquals(new Integer(1), list.getFirst());
+    }
+
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testGetFirstFailure() {
+        list.getFirst();
+    }
+
+    @org.junit.Test
+    public void testLastSuccess() {
+        list.add(1);
+        assertEquals(new Integer(1), list.getLast());
+
+        list.add(2);
+        assertEquals(new Integer(2), list.getLast());
+    }
+
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testGetLastFailure() {
+        list.getLast();
+    }
+
+    @org.junit.Test
+    public void testGetOutOfSingleElementsListSuccess() {
+        list.add(1);
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(list.get(0), list.getFirst());
+        assertEquals(list.get(list.size() - 1), list.getLast());
+    }
+
+    @org.junit.Test
+    public void testGetOutOfMultipleElementsListSuccess() {
+        list.add(1);
+        list.add(2);
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(2), list.get(list.size() - 1));
+        assertEquals(list.get(0), list.getFirst());
+        assertEquals(list.get(list.size() - 1), list.getLast());
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testGetNegativeIndexShouldFail() {
+        list.add(1);
+        assertEquals(new Integer(1), list.get(-1));
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testGetFromEmptyArrayShouldFail() {
+        assertEquals(new Integer(1), list.get(0));
+    }
+
+    @org.junit.Test
+    public void testRemoveByIndexFromSingleElementListSuccess() {
+        list.add(11);
+        assertEquals(new Integer(11), list.remove(0));
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testRemoveByIndexConsecutiveRemovalSuccess() {
+        // Need to fill and empty a list out first to ensure the node is cleaned up
+        list.add(0);
+        list.remove(0);
+        // Remove from the middle
+        list.add(11);
+        list.add(22);
+        list.add(33);
+        assertEquals(new Integer(22), list.remove(1));
+        assertEquals(2, list.size());
+        assertEquals(new Integer(11), list.get(0));
+        assertEquals(new Integer(33), list.get(1));
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveByIndexFromEmptyListShouldFail() {
+        list.remove(0);
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveByIndexWrongIndexShouldFail() {
+        list.add(1);
+        list.remove(1);
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testRemoveByIndexNegativeIndexShouldFail() {
+        list.add(1);
+        list.remove(-1);
+    }
+
+    @org.junit.Test
+    public void testRemoveByElementFromSingleElementListSuccess() {
+        list.add(11);
+        assertTrue(list.remove(new Integer(11)));
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testRemoveByElementConsecutiveRemovalSuccess() {
+        // Need to fill and empty a list out first to ensure the node is cleaned up
+        list.add(0);
+        list.remove(new Integer(0));
+        // Remove from the middle
+        list.add(11);
+        list.add(22);
+        list.add(33);
+        assertTrue(list.remove(new Integer(22)));
+        assertEquals(2, list.size());
+        assertEquals(new Integer(11), list.get(0));
+        assertEquals(new Integer(33), list.get(1));
+    }
+
+    @org.junit.Test
+    public void testRemoveByElementNonExistingShouldReturnFalse() {
+        // Empty list
+        assertFalse(list.remove(new Integer(11)));
+        // Populated
+        list.add(11);
+        assertFalse(list.remove(new Integer(22)));
+    }
+
+    @Test
+    public void testRemoveAllWithSameTypes() {
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1);
+        someList.add(22); // No exception should be thrown when calling the method
+        someList.add(3);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertTrue(list.removeAll(someList));
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testRemoveAllWithDifferentTypes() {
+        list.add(1);
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("Lisa");
+        assertFalse(list.removeAll(strList));
+    }
+
+    @Test
+    public void testRemoveAllWhenNoSuchElement() {
+        list.add(1);
+        ArrayList<Integer> anotherList = new ArrayList<>();
+        anotherList.add(333);
+        assertFalse(list.removeAll(anotherList));
+    }
+
+    @Test
+    public void testRemoveAllFromEmptyList() {
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1);
+        assertFalse(list.removeAll(someList));
+    }
+
+    @org.junit.Test
+    public void testIndexOf() {
+        assertEquals(-1, list.indexOf(1));
+
+        list.add(1);
+        assertEquals(0, list.indexOf(1));
+        assertEquals(-1, list.indexOf(2));
+
+        list.add(2);
+        assertEquals(0, list.indexOf(1));
+        assertEquals(1, list.indexOf(2));
+
+        assertEquals(-1, list.indexOf(-2));
+    }
+
+    @org.junit.Test
+    public void testLastIndexOf() {
+        assertEquals(-1, list.lastIndexOf(1));
+
+        list.add(1);
+        assertEquals(0, list.lastIndexOf(1));
+        list.add(1);
+        assertEquals(1, list.lastIndexOf(1));
+
+        list.add(2);
+        assertEquals(1, list.lastIndexOf(1));
+
+        list.add(1);
+        assertEquals(3, list.lastIndexOf(1));
+
+        assertEquals(-1, list.lastIndexOf(3));
+
+        assertEquals(-1, list.lastIndexOf(-3));
+    }
+
+    @org.junit.Test
+    public void testRetainAll() {
+        // Some should remain
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1);
+        someList.add(2);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        assertTrue(list.retainAll(someList));
+        /*
+        for (int el: list) {
+            System.out.println("---" + el);
+        }
+        */
+        // assertEquals(2, list.size()); // FIXME
+        assertEquals(new Integer(1), list.get(0));
+        assertEquals(new Integer(2), list.get(1));
+
+    }
+
+    @org.junit.Test
+    public void testRetainAllSameElementsShouldNotChange() {
+        list.add(1);
+        list.add(2);
+        // Nothing changes
+        ArrayList<Integer> anotherList = new ArrayList<>();
+        anotherList.add(1);
+        anotherList.add(2);
+        assertFalse(list.retainAll(anotherList));
+        assertEquals(2, list.size());
+    }
+
+    @org.junit.Test
+    public void testRetainAllOutOfEmptyCollection() {
+        // No exception is thrown if an empty collection is passed
+        list.add(1);
+        ArrayList<Integer> emptyList = new ArrayList<>();
+        assertTrue(list.retainAll(emptyList));
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testRetainAllNoElementsRemain() {
+        list.add(1);
+        ArrayList<Integer> otherList = new ArrayList<>();
+        otherList.add(1111);
+        assertTrue(list.retainAll(otherList));
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testRetainAllInEmptyList() {
+        // No exception is thrown if the list is empty
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1);
+        assertFalse(list.retainAll(someList));
+    }
+
+    @org.junit.Test
+    public void testContains() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertTrue(list.contains(1));
+        assertTrue(list.contains(2));
+        assertTrue(list.contains(3));
+        assertFalse(list.contains(33));
+        assertFalse(list.contains("3"));
+    }
+
+    @org.junit.Test
+    public void testContainsAllGeneralCase() {
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1);
+        someList.add(2);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertTrue(list.containsAll(someList));
+    }
+    @org.junit.Test
+    public void testContainsAllNoOccurrencesShouldBeFalse() {
+        ArrayList<Integer> someList = new ArrayList<>();
+        someList.add(1111);
+        assertFalse(list.containsAll(someList));
+    }
+
+    @org.junit.Test
+    public void testContainsAllOutOfEmptyListShouldBeFalse() {
+        ArrayList<Integer> someList = new ArrayList<>();
+        assertFalse(list.containsAll(someList));
+    }
+
+    @org.junit.Test
+    public void testIterator() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        int[] elements = new int[] {1, 2, 3};
+
+        int counter = 0;
+        for (Iterator iter = list.iterator(); iter.hasNext();) {
+            Integer element = (Integer) iter.next();
+            // Ensure a guarantee of order is provided
+            assertEquals(new Integer(elements[counter]), element);
+            counter++;
+        }
+        assertEquals(3, counter);
+
+        // Should be the same with the next loop
+        int counter2 = 0;
+        for (Integer element: list) {
+            assertEquals(new Integer(elements[counter2]), element);
+            counter2++;
+        }
+        assertEquals(3, counter2);
+    }
+
+    @org.junit.Test
+    public void testToArrayEmptyList() {
+        Object[] array = list.toArray();
+        for (int i = 0; i < list.size(); i++) {
+            assertNull(array[i]);
+        }
+    }
+
+    @org.junit.Test
+    public void testToArrayPopulatedList() {
+        list.add(1);
+        list.add(2);
+        Object[] array = list.toArray();
+        for (int i = 0; i < list.size(); i++) {
+            assertEquals(list.get(i), array[i]);
+        }
+    }
+
+    @org.junit.Test
+    public void testSubListSingleElementList() {
+        list.add(1);
+        List<Integer> newList = list.subList(0, 0);
+        assertEquals(1, newList.size());
+        assertEquals(new Integer(1), newList.get(0));
+    }
+
+    @org.junit.Test
+    public void testSubListMultipleElementsList() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        List<Integer> newList = list.subList(1, 2);
+        assertEquals(2, newList.size());
+        assertEquals(new Integer(2), newList.get(0));
+        assertEquals(new Integer(3), newList.get(1));
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testSubListEmptyListFailure() {
+        List<Integer> newList = list.subList(0, 0);
+        assertTrue(newList.isEmpty());
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testSubListWrongFirstIndexFailure() {
+        list.add(1);
+        list.subList(1, 0);
+    }
+
+    @org.junit.Test(expected = IndexOutOfBoundsException.class)
+    public void testSubListWrongSecondIndexFailure() {
+        list.add(1);
+        list.subList(0, 1);
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testSubListNegativeFirstIndexFailure() {
+        list.add(1);
+        list.subList(-1, 0);
+    }
+
+    @org.junit.Test(expected = NegativeArraySizeException.class)
+    public void testSubListNegativeSecondIndexFailure() {
+        list.add(1);
+        list.subList(0, -1);
+    }
+}
