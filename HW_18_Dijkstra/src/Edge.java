46ae491830c07fb12f3bc020db839eea2e4666c4
olenalo <uleta@ukr.net>
2018-11-17 23:54:51 +0200

    HW18: Implement Dijkstra algo

---
+++ HW_18_Dijkstra/src/Edge.java
@@ -0,0 +1,44 @@
+public class Edge {
+
+    private int fromNodeIndex;
+    private int toNodeIndex;
+    private int length;
+
+    public Edge(int fromNodeIndex, int toNodeIndex, int length) {
+        this.fromNodeIndex = fromNodeIndex;
+        this.toNodeIndex = toNodeIndex;
+        this.length = length;
+    }
+
+    public int getNeighborNodeIndex(int nodeIndex) {
+        if (this.fromNodeIndex == nodeIndex) {
+            return this.toNodeIndex;
+        } else {
+            return this.fromNodeIndex;
+        }
+    }
+
+    public int getFromNodeIndex() {
+        return fromNodeIndex;
+    }
+
+    public void setFromNodeIndex(int fromNodeIndex) {
+        this.fromNodeIndex = fromNodeIndex;
+    }
+
+    public int getToNodeIndex() {
+        return toNodeIndex;
+    }
+
+    public void setToNodeIndex(int toNodeIndex) {
+        this.toNodeIndex = toNodeIndex;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
