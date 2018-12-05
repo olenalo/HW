802bfb9f256f7757cd1ab2e8833997e9fdee9cee
olenalo <uleta@ukr.net>
2018-12-05 17:55:14 +0200

    Fix queries

--- HW_23_EmployeesApp/src/main/java/Demo.java
+++ HW_23_EmployeesApp/src/main/java/Demo.java
@@ -1,45 +1,47 @@
 import employeesApp.EmployeesApp;
 import utils.EmployeesPrintUtils;
 
+
 public class Demo {
 
     public static void main(String[] args) {
+        // Print first `limit` entries TODO consider passing to queries (conditionally)
+        int limit = 3;  // Integer.MAX_VALUE;
+
         /*
         System.out.println("\n------------------------------\nChecking departments...");
         System.out.println("All deps: " + EmployeesApp.getAllDepartments());
         System.out.println("All deps ordered: " + EmployeesApp.getAllOrderedDepartments("dept_name"));
         */
 
-        /*
         System.out.println("\n------------------------------\nChecking employees...");
-        System.out.println("All employees: " + EmployeesApp.getAllEmployees());
-        System.out.println("All employees ordered: " + EmployeesApp.getAllOrderedEmployees("last_name"));
-        */
+        // System.out.println("All employees: " + EmployeesApp.getAllEmployees());
+        // System.out.println("All employees ordered: " + EmployeesApp.getAllOrderedEmployees("last_name"));
 
-        // FIXME total salaries, not current!
         System.out.println("#1. All employees, indicating total salary and title: ");
-        //EmployeesPrintUtils.printListSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary());
+        EmployeesPrintUtils.printListTotalSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary(), limit);
 
         System.out.println("#2. Employees hired in specific year, indicating department and hire date: ");
-        // EmployeesPrintUtils.printListDeptHiredSpecifiedYear(EmployeesApp.getEmployeesHiredInYear(2000)); // TODO user input
-
-        System.out.println("#3. Raise salaries for selected employees and print results: ");
-        // TODO do with a stored procedure e.g. https://stackoverflow.com/a/24799562
         // TODO user input
-        double totalSalaryEarnedBoundary = 80000;  // FIXME select by total salaries, not current!
+        int year = 2000;
+        EmployeesPrintUtils.printListDeptHiredSpecifiedYear(EmployeesApp.getEmployeesHiredInYear(year), limit);
+
+        System.out.println("#3. Raise current salaries for selected employees and print results: ");
+        // TODO user input
+        double totalSalaryEarnedThreshold = 1000000;
         double salaryRaise = 1000;
-        System.out.println("Current salaries before the raise...");
-        EmployeesPrintUtils.printListSalaryTitle(
-                EmployeesApp.getSelectedEmployeesWithTitleCurrentSalary(totalSalaryEarnedBoundary)
-        );
-        EmployeesApp.updateEmployeesWithSalaryBySecificValue(totalSalaryEarnedBoundary, salaryRaise);
-        System.out.println("Current salaries after the raise...");
-        EmployeesPrintUtils.printListSalaryTitle(
-                EmployeesApp.getSelectedEmployeesWithTitleCurrentSalary(totalSalaryEarnedBoundary)
-        );
+
+        System.out.println("Current salaries before the raise..."); // debug purposes
+        EmployeesPrintUtils.printListCurrentSalaryTitle(EmployeesApp.getAllEmployeesCurrentSalary(), limit);
+
+        System.out.println("Updating selected employees' current salaries...");
+        EmployeesApp.updateEmployeesWithSalaryBySpecificValue(totalSalaryEarnedThreshold, salaryRaise);
+
+        System.out.println("Current salaries after the raise..."); // debug purposes
+        EmployeesPrintUtils.printListCurrentSalaryTitle(EmployeesApp.getAllEmployeesCurrentSalary(), limit);
+
         // Print what's required
-        EmployeesPrintUtils.printListSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary());
-
+        System.out.println("Total salaries and titles...");
+        EmployeesPrintUtils.printListTotalSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary(), limit);
     }
-
 }
--- HW_23_EmployeesApp/src/main/java/employeesApp/EmployeesApp.java
+++ HW_23_EmployeesApp/src/main/java/employeesApp/EmployeesApp.java
@@ -40,19 +40,17 @@ public class EmployeesApp {
         return emplDao.getEmployeesHiredInYear(year);
     }
 
-    public static List<Employee> getSelectedEmployeesWithTitleCurrentSalary(double filterTotalSalaryEarned) {
-        return emplDao.getSelectedWithTitleCurrentSalary(filterTotalSalaryEarned);
+    public static List<Employee> getAllEmployeesCurrentSalary() {
+        return emplDao.getAllWithTitleCurrentSalary();
     }
 
-    public static void updateEmployeesWithSalaryBySecificValue(double filterTotalSalaryEarned,
-                                                               double salaryRaiseValue) {
-        emplDao.updateEmployeesWithSalaryBySpecificValue(filterTotalSalaryEarned, salaryRaiseValue);
-    }
-
-
-
-    public static List<Employee> get() {
-        return emplDao.getAllWithTitleTotalSalary();
+    public static void updateEmployeesWithSalaryBySpecificValue(double filterTotalSalaryEarned,
+                                                                double salaryRaiseValue) {
+        List<Employee> selectedEmployees = emplDao.getEmployeesSalaryThreshold(filterTotalSalaryEarned);
+        // It takes a long, long time... TODO consider speeding up
+        for (Employee employee : selectedEmployees) {
+            emplDao.updateEmployeeRaiseCurrentSalary(employee.getId(), salaryRaiseValue);
+        }
     }
 
 }
