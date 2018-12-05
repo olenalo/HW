8cf8f802796050870181bfaee72e7fe03047be1c
olenalo <uleta@ukr.net>
2018-12-05 18:09:03 +0200

    Add user input, add static imports

--- HW_23_EmployeesApp/src/main/java/Demo.java
+++ HW_23_EmployeesApp/src/main/java/Demo.java
@@ -1,6 +1,11 @@
-import employeesApp.EmployeesApp;
-import utils.EmployeesPrintUtils;
-
+import static employeesApp.EmployeesApp.getAllEmployeesWithTitleTotalSalary;
+import static employeesApp.EmployeesApp.getEmployeesHiredInYear;
+import static employeesApp.EmployeesApp.getAllEmployeesCurrentSalary;
+import static employeesApp.EmployeesApp.updateEmployeesWithSalaryBySpecificValue;
+import static utils.EmployeesPrintUtils.printListCurrentSalaryTitle;
+import static utils.EmployeesPrintUtils.printListDeptHiredSpecifiedYear;
+import static utils.EmployeesPrintUtils.printListTotalSalaryTitle;
+import static utils.IOUtils.getHireYear;
 
 public class Demo {
 
@@ -8,40 +13,36 @@ public class Demo {
         // Print first `limit` entries TODO consider passing to queries (conditionally)
         int limit = 3;  // Integer.MAX_VALUE;
 
-        /*
-        System.out.println("\n------------------------------\nChecking departments...");
-        System.out.println("All deps: " + EmployeesApp.getAllDepartments());
-        System.out.println("All deps ordered: " + EmployeesApp.getAllOrderedDepartments("dept_name"));
-        */
+        // System.out.println("\n------------------------------\nChecking departments...");
+        // System.out.println("All deps: " + getAllDepartments());
+        // System.out.println("All deps ordered: " + getAllOrderedDepartments("dept_name"));
 
         System.out.println("\n------------------------------\nChecking employees...");
-        // System.out.println("All employees: " + EmployeesApp.getAllEmployees());
-        // System.out.println("All employees ordered: " + EmployeesApp.getAllOrderedEmployees("last_name"));
+        // System.out.println("All employees: " + getAllEmployees());
+        // System.out.println("All employees ordered: " + getAllOrderedEmployees("last_name"));
 
         System.out.println("#1. All employees, indicating total salary and title: ");
-        EmployeesPrintUtils.printListTotalSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary(), limit);
+        printListTotalSalaryTitle(getAllEmployeesWithTitleTotalSalary(), limit);
 
         System.out.println("#2. Employees hired in specific year, indicating department and hire date: ");
-        // TODO user input
-        int year = 2000;
-        EmployeesPrintUtils.printListDeptHiredSpecifiedYear(EmployeesApp.getEmployeesHiredInYear(year), limit);
+        int year = getHireYear();  // 2000
+        printListDeptHiredSpecifiedYear(getEmployeesHiredInYear(year), limit);
 
         System.out.println("#3. Raise current salaries for selected employees and print results: ");
-        // TODO user input
         double totalSalaryEarnedThreshold = 1000000;
         double salaryRaise = 1000;
 
         System.out.println("Current salaries before the raise..."); // debug purposes
-        EmployeesPrintUtils.printListCurrentSalaryTitle(EmployeesApp.getAllEmployeesCurrentSalary(), limit);
+        printListCurrentSalaryTitle(getAllEmployeesCurrentSalary(), limit);
 
         System.out.println("Updating selected employees' current salaries...");
-        EmployeesApp.updateEmployeesWithSalaryBySpecificValue(totalSalaryEarnedThreshold, salaryRaise);
+        updateEmployeesWithSalaryBySpecificValue(totalSalaryEarnedThreshold, salaryRaise);
 
         System.out.println("Current salaries after the raise..."); // debug purposes
-        EmployeesPrintUtils.printListCurrentSalaryTitle(EmployeesApp.getAllEmployeesCurrentSalary(), limit);
+        printListCurrentSalaryTitle(getAllEmployeesCurrentSalary(), limit);
 
         // Print what's required
         System.out.println("Total salaries and titles...");
-        EmployeesPrintUtils.printListTotalSalaryTitle(EmployeesApp.getAllEmployeesWithTitleTotalSalary(), limit);
+        printListTotalSalaryTitle(getAllEmployeesWithTitleTotalSalary(), limit);
     }
 }
