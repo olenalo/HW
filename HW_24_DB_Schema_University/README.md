fc9eb39fb17b02f392d29323139fe5cffd44307d
olenalo <olenalo@users.noreply.github.com>
2018-12-05 23:32:49 +0200

    HW24: Add University DB Schema (MySQL) (#14)
    * Add MySQL queries to create the DB and tables
    * Add DB schema

---
+++ HW_24_DB_Schema_University/README.md
@@ -0,0 +1,13 @@
+These MySQL queries allow for creating `university` database and tables with regard to [BackToSchool models](https://github.com/olenalo/HW/tree/4ce5c3d832983ce953535e563ecad768d4432af6/HW_19_IO/src/BackToSchool/models) (HW Nno.14,19). 
+
+Also for reference: [Entry Exams System](https://github.com/olenalo/HW/tree/5ec5da5a859e4005b002a388d1a23b3d067d9cdd/HW_06_Entry%20Exams%2C%20Tower%20of%20Hanoi%2C%20Clocks/EntryExamsSystem/src/com/company) (HW No.06).
+
+Auto-generated DB Schema:
+
+![Schema](resources/university_auto_schema.png)
+
+DB Schema modelling (TODO is resolved, see the autoschema with pivot tables above): 
+
+![Schema](resources/university_schema.jpg)
+
+Editable version: [Draw.io schema](https://www.draw.io/#G1LahKoBsn8t3SkdzTJJRNGqKZmLlZ6oj-)
---
+++ HW_24_DB_Schema_University/university_queries.sql
@@ -0,0 +1,110 @@
+create database university;
+
+create table university.statuses (
+    status_id int(11) not null auto_increment,
+    title varchar(20) not null,
+    primary key (status_id)
+);
+
+create table university.faculties (
+    faculty_id int(11) not null auto_increment,
+    title varchar(20) not null,
+    primary key (faculty_id)
+);
+
+create table university.instructors (
+    instructor_id int(11) not null auto_increment,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    dob date,
+    status_id int(11),
+    faculty_id int(11),
+    course_id int(11),
+    primary key (instructor_id),
+    foreign key (status_id) references university.statuses(status_id),
+    foreign key (faculty_id) references university.faculties(faculty_id)
+);
+
+create table university.courses (
+    course_id int(11) not null auto_increment,
+    title varchar(20) not null,
+    # instructor_id int(11),
+    faculty_id int(11),
+    primary key (course_id),
+    foreign key (faculty_id) references university.faculties(faculty_id)
+    # foreign key (instructor_id) references university.instructors(instructor_id)
+);
+
+create table university.exams (
+    exam_id int(11) not null auto_increment,
+    course_id int(11) not null,
+    # instructor_id int(11),
+    date_held date not null,
+    primary key (exam_id),
+    foreign key (course_id) references university.courses(course_id)
+    # foreign key (instructor_id) references university.instructors(instructor_id)
+);
+
+create table university.students (
+    student_id int(11) not null auto_increment,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    dob date not null,
+    country varchar(20),
+    current_year int(4),
+    entry_year int(4),
+    faculty_id int(11),
+    # course_id int(11),
+    primary key (student_id),
+    foreign key (faculty_id) references university.faculties(faculty_id)
+    # foreign key (course_id) references university.courses(course_id)
+);
+
+create table university.grades (
+    grade_id int(11) not null auto_increment,
+    student_id int(11) not null,
+    exam_id int(11),
+    instructor_id int(11),
+    date_assigned date,
+    grade float not null,
+    primary key (grade_id),
+    foreign key (instructor_id) references university.instructors(instructor_id),
+    foreign key (student_id) references university.students(student_id),
+    foreign key (exam_id) references university.exams(exam_id)
+);
+
+# Create intermediate (pivot) tables
+
+create table university.students_courses (
+    entry_id int(11) not null auto_increment,
+    course_id int(11) not null,
+    student_id int(11) not null,
+    primary key (entry_id),
+    foreign key (student_id) references university.students(student_id),
+    foreign key (course_id) references university.courses(course_id)
+);
+
+create table university.instructors_courses (
+    entry_id int(11) not null auto_increment,
+    course_id int(11) not null,
+    instructor_id int(11) not null,
+    primary key (entry_id),
+    foreign key (instructor_id) references university.instructors(instructor_id),
+    foreign key (course_id) references university.courses(course_id)
+);
+
+
+create table university.instructors_exams (
+    entry_id int(11) not null auto_increment,
+    instructor_id int(11) not null,
+    exam_id int(11),
+    primary key (entry_id),   
+    foreign key (instructor_id) references university.instructors(instructor_id),
+    foreign key (exam_id) references university.exams(exam_id)
+);
+
+
+# describe university.exams;
+# drop database university;
+
+# TODO Populate a bit
